c4993e862b819b5f2faceca3e6af693e64ef2bb8
Markus Huggler <markus.huggler@ecologic.ch>
2020-06-01 22:40:19 +0200

    chore: disables debug output

--- internal/time/time.go
+++ internal/time/time.go
@@ -1,7 +1,6 @@
 package time
 
 import (
-	"fmt"
 	"log"
 	"time"
 )
@@ -10,8 +9,6 @@ func NeedsNotification(reminderString string) (result bool, err error) {
 	now := time.Now()
 	zone, _ := now.Zone()
 	reminder, err := time.Parse("2006-01-02T15:05 MST", reminderString + " " + zone)
-	fmt.Printf("Now     : %v\n", now)
-	fmt.Printf("Reminder: %v\n", reminder)
 	if err != nil {
 		log.Printf("Could not parse date %v\n", reminderString)
 	}

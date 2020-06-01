d9878fb44adbff4f92fedee4d25adcc326964957
Markus Huggler <markus.huggler@ecologic.ch>
2020-06-01 22:19:55 +0200

    chore: enables debug output

--- internal/time/time.go
+++ internal/time/time.go
@@ -10,8 +10,8 @@ func NeedsNotification(reminderString string) (result bool, err error) {
 	now := time.Now()
 	zone, _ := now.Zone()
 	reminder, err := time.Parse("2006-01-02T15:05 MST", reminderString + " " + zone)
-	fmt.Printf("Now     : %v", now)
-	fmt.Printf("Reminder: %v", reminder)
+	fmt.Printf("Now     : %v\n", now)
+	fmt.Printf("Reminder: %v\n", reminder)
 	if err != nil {
 		log.Printf("Could not parse date %v\n", reminderString)
 	}
